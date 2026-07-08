3f0d6796473a56234f35df049a9674f7bee649d2
kurosehead <85331598+kurosehead@users.noreply.github.com>
2026-07-08 16:10:26 +0900

    feat:visitページもうちょい

--- page.php
+++ page.php
@@ -7,25 +7,63 @@ get_header(); ?>
 <!-- ページヘッダー -->
 <div class="lower-header">
   <div class="lower-header__inner container">
-    <?php
-    $page_title_en = strtoupper(str_replace('-', ' ', get_post_field('post_name', get_the_ID())));
-    ?>
-    <p class="lower-header__en"><?php echo esc_html($page_title_en); ?></p>
+    <p class="lower-header__en"><?php the_field('page_title_en') ?></p>
     <p class="lower-header__ja"><?php the_title(); ?></p>
   </div>
 </div>
-<main class="<?php if (is_page('visit')) {
-  echo 'style-general';
-} ?>">
-  <?php if (have_posts()): ?>
-    <?php while (have_posts()):
-      the_post(); ?>
-      <?php the_content(); ?>
-    <?php endwhile; ?>
+<?php
+$layout = get_field('sidebar_option');
+if ($layout === 'on'): //sidebarがあるときだけmainとsidebarをdivで囲む
+  ?>
+  <div class="main-aside--wrapper">
+
+    <button class="btn-index" id="btn-index">目次</button>
+    <div class="aside-overlay" id="aside-overlay"></div>
+
+    <!-- サイドバー -->
+    <aside class="aside-left <?php if(get_field('sidebar_color')) {echo 'aside-'.get_field('sidebar_color');} ?>">
+      <p class="aside__logo">
+        <a href="<?php echo home_url(); ?>">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/logo-color.svg" alt="大城フ科クリニック">
+        </a>
+      </p>
+      <!-- <div class="btn-back">
+        <a href="#">一覧ページに戻る</a>
+      </div> -->
+      <?php echo wp_kses_post(get_field('sidebar_code')); ?>
+      <button class="aside-close" id="aside-close">
+      </button>
+
+    </aside>
   <?php endif; ?>
 
-  <?php breadcrumb(); //パンくず ?>
-</main>
+  <main class="<?php if (is_page('visit')) {
+    echo 'style-general';
+  } ?>">
+    <?php if (have_posts()): ?>
+
+      <?php if ($layout === 'on'): //sidebarがあるときだけページヘッダーが2回登場 ?>
+        <!-- ページヘッダー -->
+        <div class="lower-header">
+          <div class="lower-header__inner container">
+            <p class="lower-header__en"><?php the_field('page_title_en') ?></p>
+            <h1 class="lower-header__ja"><?php the_title(); ?></h1>
+          </div>
+        </div>
+      <?php endif; ?>
+
+      <?php while (have_posts()):
+        the_post(); ?>
+        <?php the_content(); ?>
+      <?php endwhile; ?>
+    <?php endif; ?>
+
+    <?php breadcrumb(); //パンくず ?>
+  </main>
+
+  <?php if ($layout === 'on'): //sidebarがあるときだけmainとsidebarをdivで囲む ?>
+  </div>
+  <?php endif; ?>
 
 <?php if (is_page('access')): ?>
   <?php get_template_part('template-parts/page-access'); ?>
